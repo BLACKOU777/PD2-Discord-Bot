97c1487603326812bcb3856127bc4d73af27f713
BLACKOU777 <64636456+BLACKOU777@users.noreply.github.com>
2020-05-01 11:26:57 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,5 @@
 import discord
 from discord.ext import commands, tasks
-import os
 
 TOKEN = 'NzA1Mjc1MTg4Njk0MDg5NzQ4.XqpVrg.X2hBJE4lXdYJoPTS2y4NoZ0q2ko'
 
@@ -13,17 +12,5 @@ async def on_ready():
 	print('Bot is Online.')
 
 #                          Commands
-
-@client.command()
-async def load (ctx, extension):
-	client.load_extension(f'cogs.{extension}')
-
-@client.command()
-async def unload (ctx, extension):
-	client.unload_extension(f'cogs.{extension}')
-
-for filename in os.listdir('./cogs'):
-	if filename.endswith('.py'):
-		client.load_extension(f'cogs.{filename[:-3]}')
 
 client.run(TOKEN)
