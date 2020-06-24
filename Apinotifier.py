9a4dd053d1700f753103640f49f18587262a15d8
BLACKOU777 <64636456+BLACKOU777@users.noreply.github.com>
2020-06-24 20:14:17 +0100

    Update Apinotifier.py

--- Apinotifier.py
+++ Apinotifier.py
@@ -2,10 +2,10 @@ import requests, json, re, discord
 from datetime import datetime, timedelta
 from dhooks import Webhook
 
-pd2_server = Webhook('https://discordapp.com/api/webhooks/710131681012482141/iDdoBvAUPC0WYHJYWKJ9Ogmna6hpwPR0ZySkK-VqXFvaG4WFYbymQ74E4qlH8HLEL-Rf')
-test_server = Webhook('https://discordapp.com/api/webhooks/713960600572788807/5hYpO46jd5rkM1MU5xRLqZ_hTQ_9sInGNRrpaeK4V0qkJTIiLvWkjVfTHGyDTOVNrYy3')
+pd2_server = Webhook()
+test_server = Webhook()
 hook = pd2_server
-response = requests.get("https://www.speedrun.com/api/v1/notifications", headers={'X-API-Key': 's89karnzlc4hfnkkb6sg0nxls'})
+response = requests.get("https://www.speedrun.com/api/v1/notifications", headers={'X-API-Key': ''})
 r_json = response.json()
 for data in r_json['data']:
     created = data['created']
